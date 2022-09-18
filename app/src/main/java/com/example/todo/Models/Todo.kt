ad8a671d2b38ecbfbb73dc14f78cd3e8f7b5db87
Conny Hansson <conhan@hotmail.se>
2022-09-18 17:57:19 +0200

    Update todo model as entity
    Change todo model into a room entity with a primary key

--- app/src/main/java/com/example/todo/Models/Todo.kt
+++ app/src/main/java/com/example/todo/Models/Todo.kt
@@ -1,6 +1,11 @@
 package com.example.todo.Models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class Todo(
     var title : String,
-    var body : String
+    var body : String,
+    @PrimaryKey val id : Int? = null
 )
