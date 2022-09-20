50f35d49d1a275a67985ee47c1847792123e4ce8
Conny Hansson <conhan@hotmail.se>
2022-09-20 18:28:03 +0200

    Nav controller
    Implement app navigation with a navcontroller

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -3,56 +3,44 @@ package com.example.todo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.NavArgument
+import androidx.navigation.NavType
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
+import com.example.todo.Util.Routes
 import com.example.todo.Views.TodoItem
-
 import com.example.todo.Views.TodoList
-import com.example.todo.state.State
 import com.example.todo.ui.theme.TodoTheme
 
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
 
-
-
         setContent {
-
-            var state = remember { mutableStateOf<State>(State.ShowList)}
+            val navController = rememberNavController()
 
             TodoTheme {
-                // A surface container using the 'background' color from the theme
-                when(state.value) {
-                    is State.ShowList -> TodoList(state)
-                    is State.Item -> TodoItem(state)
-                    is State.Error -> Text("error ${(state.value as State.Error).cause}")
+                NavHost(navController = navController , startDestination = Routes.TODO_LIST ) {
+                    composable(Routes.TODO_LIST) { TodoList { navController.navigate(it.route) } }
+                    composable(
+                        route = Routes.TODO_ITEM + "?todoId={todo_id}",
+                        arguments = listOf(
+                            navArgument("todo_id") {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            }
+                        )
+                    ) { TodoItem { navController.popBackStack() } }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TodoTheme {
-        Greeting("Android")
     }
 }
