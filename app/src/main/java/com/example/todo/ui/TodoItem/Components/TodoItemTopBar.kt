9b7f2e18d071ba637b35a352692281a605e9dce3
Conny Hansson <conhan@hotmail.se>
2022-10-05 16:58:24 +0200

    Todoitem top bar
    Implement a top bar with different options and appearances
    depending on if the user is editing the todo or viewing it

---
+++ app/src/main/java/com/example/todo/ui/TodoItem/Components/TodoItemTopBar.kt
@@ -0,0 +1,73 @@
+package com.example.todo.ui.TodoItem.Components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.List
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.todo.ui.TodoItem.TodoItemEvent
+import com.example.todo.ui.TodoItem.TodoItemVM
+
+
+@Composable
+fun TodoItemTopBar(
+    todoTitle : String,
+    editState: TodoItemVM.TodoState,
+    onEvent: (TodoItemEvent) -> Unit
+) {
+
+    val padding = Modifier.padding(3.dp)
+
+    TopAppBar(
+        backgroundColor = MaterialTheme.colors.primary,
+        elevation = 3.dp,
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (editState == TodoItemVM.TodoState.EDIT) {
+                val buttonWeight = Modifier.weight(1f)
+                TextField(
+                    value = todoTitle,
+                    onValueChange = { onEvent(TodoItemEvent.setTitle(it)) },
+                    modifier = padding.weight(3f)
+                )
+                IconButton(
+                    onClick = { onEvent(TodoItemEvent.saveTodo) },
+                    modifier = padding.then(buttonWeight)
+                ) {
+                    Icon(imageVector = Icons.Filled.Add, contentDescription = "save todo")
+                }
+                IconButton(
+                    onClick = { /*TODO*/ },
+                    modifier = padding.then(buttonWeight)
+                ) {
+                    Icon(imageVector = Icons.Filled.List, "Todo Menu")
+                }
+
+            } else {
+                Text(
+                    text = todoTitle,
+                    modifier = padding
+                )
+                Button(
+                    onClick = { onEvent(TodoItemEvent.setEditState(TodoItemVM.TodoState.EDIT))},
+                    colors = ButtonDefaults.buttonColors(MaterialTheme.colors.primaryVariant),
+                    modifier = padding
+                ) {
+                    Icon(imageVector = Icons.Filled.Edit, contentDescription = null)
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/todo/ui/TodoItem/TodoItemScreen.kt
+++ app/src/main/java/com/example/todo/ui/TodoItem/TodoItemScreen.kt
@@ -1,21 +1,16 @@
 package com.example.todo.Views
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.*
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 import com.example.todo.Util.UIEvent
+import com.example.todo.ui.TodoItem.Components.TodoItemTopBar
 import com.example.todo.ui.TodoItem.TodoItemEvent
 import com.example.todo.ui.TodoItem.TodoItemVM
 
@@ -38,7 +33,11 @@ fun TodoItemScreen(
 
 
     Scaffold(
-        topBar = { topBar(todoTitle = viewModel._title, onEvent = viewModel::onEvent ) }
+        topBar = { TodoItemTopBar(
+            todoTitle = viewModel.title,
+            onEvent = viewModel::onEvent,
+            editState = viewModel.state
+        ) }
     ){
         Column(
             modifier = Modifier.fillMaxSize()
@@ -46,34 +45,12 @@ fun TodoItemScreen(
             TextField(
                 modifier = Modifier
                     .fillMaxWidth(),
-                value = viewModel._body,
+                value = viewModel.body,
                 onValueChange = { viewModel.onEvent(TodoItemEvent.setBody(it)) }
             )
         }
     }
 
-}
-
-@Composable
-fun topBar(
-    todoTitle : String,
-    onEvent: (TodoItemEvent) -> Unit
-) {
-    TopAppBar(
-        backgroundColor = MaterialTheme.colors.primary,
-        elevation = 3.dp,
-    ) {
-        TextField(
-            value = todoTitle,
-            onValueChange = { onEvent(TodoItemEvent.setTitle(it)) }
-        )
-        Button(onClick = { onEvent(TodoItemEvent.saveTodo) }) {
-            Icon(imageVector = Icons.Filled.Add, contentDescription = "save todo")
-        }
-        Button(onClick = { /*TODO*/ }) {
-            Icon(imageVector = Icons.Filled.Menu, "Todo Menu")
-        }
-    }
 }
 
 
