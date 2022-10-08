8d4f04f1c2ca044dcb94a469cd3cb00a7ccd01f6
Conny Hansson <conhan@hotmail.se>
2022-10-08 17:56:34 +0200

    Create notifications channel
    Create notification channel for todos on app startup

--- app/src/main/java/com/example/todo/TodoApp.kt
+++ app/src/main/java/com/example/todo/TodoApp.kt
@@ -1,7 +1,34 @@
 package com.example.todo
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import com.example.todo.Models.Services.TodoNotificationServiceImpl
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TodoApp : Application()
+class TodoApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        createTodoNotificationChannel()
+    }
+
+    private fun createTodoNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val todoReminderChannel = NotificationChannel(
+                TodoNotificationServiceImpl.TODO_REMINDER_CHANNEL_ID,
+                "Todo Reminder",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            todoReminderChannel.description = "Used to issue reminders of todos"
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(todoReminderChannel)
+        }
+    }
+}
