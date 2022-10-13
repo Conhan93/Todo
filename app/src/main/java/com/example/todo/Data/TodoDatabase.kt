07d15dcc31241619219fc21b56e7f6eae083eb14
Conny Hansson <conhan@hotmail.se>
2022-10-13 16:44:11 +0200

    Update database
    update room database with rmeinder notification entity

--- app/src/main/java/com/example/todo/Data/TodoDatabase.kt
+++ app/src/main/java/com/example/todo/Data/TodoDatabase.kt
@@ -2,9 +2,15 @@ package com.example.todo.Data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.todo.Models.ReminderNotification
 import com.example.todo.Models.Todo
 
-@Database(entities = [Todo::class], version = 1, exportSchema = false)
+@Database(
+    entities = [Todo::class, ReminderNotification::class],
+    version = 2,
+    exportSchema = false
+)
 abstract class TodoDatabase : RoomDatabase() {
-    abstract val dao : TodoDAO
+    abstract val todoDAO: TodoDAO
+    abstract val reminderDAO: ReminderDAO
 }
