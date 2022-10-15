398c7faea5b86d5f8ab7b450d0f1f61adf65ea71
Conny Hansson <conhan@hotmail.se>
2022-10-15 12:13:08 +0200

    Reminder card
    Composable to display reminder time information on the reminders
    screen list

---
+++ app/src/main/java/com/example/todo/ui/TodoItemReminders/Components/ReminderCard.kt
@@ -0,0 +1,68 @@
+package com.example.todo.ui.TodoItemReminders.Components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.todo.Models.ReminderNotification
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+
+@Composable
+fun ReminderCard(
+    reminderNotification: ReminderNotification,
+    modifier: Modifier = Modifier
+) {
+    val date = ZonedDateTime.parse(reminderNotification.date)
+    val contentPadding = Modifier.padding(4.dp)
+
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .then(modifier),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "${date.hour}.${date.minute}",
+            style = TextStyle(
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 30.sp
+            ),
+            modifier = contentPadding
+        )
+        Text(
+            text = date.toLocalDate().format(DateTimeFormatter.ISO_DATE),
+            style = TextStyle(
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 30.sp
+            ),
+            modifier = contentPadding
+        )
+    }
+}
+
+@Composable
+@Preview
+fun ReminderCardPreview() {
+    val dummyReminder = ReminderNotification(
+        "foo",
+        "bar",
+        UUID.randomUUID().toString(),
+        todoId = 1,
+        date = ZonedDateTime.now().toString()
+    )
+
+    ReminderCard(reminderNotification = dummyReminder)
+}
--- app/src/main/java/com/example/todo/ui/TodoItemReminders/TodoItemRemindersScreen.kt
+++ app/src/main/java/com/example/todo/ui/TodoItemReminders/TodoItemRemindersScreen.kt
@@ -1,9 +1,14 @@
 package com.example.todo.ui.TodoItemReminders
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +19,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.todo.Util.UIEvent
 import com.example.todo.ui.TodoItem.TodoItemVM
+import com.example.todo.ui.TodoItemReminders.Components.ReminderCard
 import com.example.todo.ui.TodoItemReminders.Components.TodoItemRemindersTopBar
 import com.example.todo.ui.TodoList.SwipeableListItem
 
@@ -40,11 +46,19 @@ fun TodoItemRemindersScreen(
     ) {
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(4.dp),
-            contentPadding = PaddingValues(start = 4.dp)
+            //modifier = Modifier.padding(top = 4.dp, bottom = 4.dp),
+            contentPadding = PaddingValues(4.dp)
         ) {
             items(reminders.value, key = {it.id!!}) {
                 SwipeableListItem(onDismiss = { viewModel.onEvent(TodoItemRemindersEvent.DeleteEvent(it)) }) {
-                    Text(it.date)
+                    ReminderCard(
+                        reminderNotification = it,
+                        modifier = Modifier
+                            .background(
+                                MaterialTheme.colors.primary,
+                                shape = RoundedCornerShape(4.dp)
+                            )
+                    )
                 }
             }
         }
