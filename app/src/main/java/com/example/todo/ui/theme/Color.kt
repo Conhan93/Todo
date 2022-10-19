6404242580ef8df698d0af08bad84423daea1fc2
Conny Hansson <conhan@hotmail.se>
2022-10-19 14:12:25 +0200

    Set app colours

--- app/src/main/java/com/example/todo/ui/theme/Color.kt
+++ app/src/main/java/com/example/todo/ui/theme/Color.kt
@@ -2,6 +2,14 @@ package com.example.todo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val lightGreen400 = Color(0xFF9CCC65)
+val lightGreen700 = Color(0xFF689F38)
+val lightGreen500 = Color(0xFF8BC34A)
+
+val primary = Color(0xFF9CCC65)
+val primaryDark = Color(0xFF6B9B37)
+val primaryLight = Color(0xFFCFFF95)
+
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
